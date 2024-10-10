591e4282b3c0ef2d6481e2f2be1e725aeaa94884
gokul3955 <gokul.reddy@vmokshagroup.com>
2024-10-10 17:43:59 +0530

    Initial commit

---
+++ APP.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
